32700bc1a865c01cb7c0e357456c921e3b653598
Roshan Tadadikar <71004273+Roshan-Tadadikar@users.noreply.github.com>
2022-04-08 12:55:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  const mystyle={
+    fontFamily: "Product Sans"
+  };
+  return (
+    <h1 className="text-5xl font-bold  text-center mt-60" style={mystyle}>
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;

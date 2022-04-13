6ff3c8799f1a36136595d83ff177e325fca09016
abdullah nore <49518940+abdullahnore@users.noreply.github.com>
2022-04-13 09:18:37 +0530

    routing
    /Login for login page /Signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
-import Login from './Components/Login';
+import "./App.css";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-   <div className='App'>
-     <Login />
-   </div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 

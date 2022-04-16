6f5084f74b3cfc5378c75f8531b984dc9d7aa31e
Roshan Tadadikar <71004273+Roshan-Tadadikar@users.noreply.github.com>
2022-04-16 16:47:05 +0530

    c commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contactus from './Components/Contact';
 
 function App() {
   return (
@@ -10,8 +11,10 @@ function App() {
         <Routes>
           <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
+          <Route path="/Contact" element={<Contactus />} />
         </Routes>
       </Router>
+      
     </div>
   );
 }

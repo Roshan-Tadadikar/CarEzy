007d5a2722dd9740aacd943d091551ffb61f5664
abdullah nore <49518940+abdullahnore@users.noreply.github.com>
2022-04-21 16:02:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
+import Nav from "./Components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Router>
         <Routes>
           <Route path="/Login" element={<Login />} />

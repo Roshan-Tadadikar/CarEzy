2f09dddd4ddd9213aa341dd9266cbee3799ab5dc
abdullah nore <49518940+abdullahnore@users.noreply.github.com>
2022-04-24 18:12:17 +0530

    modal singup
    responsive ness and outer area click close is remaining

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
-// import Nav from "./Components/Nav";
+import Nav from "./Components/Nav";
 import Contact from "./Components/Contact";
 // import Home from "./Components/Home";
 import Hometwo from "./Components/Hometwo";
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Hometwo />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
+          {/* <Route path="/Signup" element={<Signup />} /> */}
           <Route path="/Contact" element={<Contact />} />
         </Routes>
       </Router>
